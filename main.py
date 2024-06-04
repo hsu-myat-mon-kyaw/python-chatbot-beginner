56e8bb70dffa620fbd7d7d7053b47393ff4d928b
hsu-myat-mon-kyaw <122143221+hsu-myat-mon-kyaw@users.noreply.github.com>
2024-06-04 11:03:01 +0630

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#simple chat bot
+
+#Welcome to the chat bot
+print("Welcome I am the chat bot 3000!")
+
+#Ask for the user name
+userName = input("What is your name?")
+print("Nice to meet you, " + userName + "!")
+
+if userName == "Noah" :
+  print("My best friend's name is Noah!")
+else: 
+  print("I hope you're having a great day!")
